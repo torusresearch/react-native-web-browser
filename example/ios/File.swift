e3112c99a1859b1b452419d8ca524427c2f5f7a3
michaellee8 <ckmichael8@gmail.com>
2022-04-14 11:42:33 +0800

    chore: remove original example code

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  WebBrowserExample
-//
-
-import Foundation
--- example/ios/WebBrowserExample/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  WebBrowserExample
-//
-//  Created by Michael Lee on 14/4/2022.
-//
-
-import Foundation
